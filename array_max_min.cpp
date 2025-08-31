eb07d46370d349ec6fda24f87327734d7d8022aa
Ansh Gupta <ansh16881@gmail.com>
2025-08-31 23:46:32 +0530

    Update array_max_min.cpp

--- array_max_min.cpp
+++ array_max_min.cpp
@@ -7,22 +7,7 @@ int main() {
     cout << "ENTER 5 values" << endl;
     for (int i = 0; i < 5; i++) {
         cin >> arr[i];
-    }
 
-    int max = arr[0];
-    int min = arr[0];
-
-    for (int i = 0; i < 5; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        }
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-cout<<"the output of the max and min is ";
-    cout << "Min value is: " << min << endl;
-    cout << "Max value is: " << max;
 
     return 0;
 }
